8061d84ab515db178a1e3c0ed0c5161d12c56955
ViniciusVicentini <vinisvicentini@gmail.com>
2026-03-09 20:35:09 -0300

    alteracoes

--- aula03/exemplo3.php
+++ aula03/exemplo3.php
@@ -1,9 +1,37 @@
-<?php
-    $a = "Casal 20";
-    $b = 10;
-    $c = $a + $b;
-    echo $c
-
-    $pre = "hiper";
-    echo $pre . $hiper
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividades</title>
+</head>
+<body>
+    <h1>Exercício 7</h1>
+    <?php
+         $a = "Casal 20";
+         $b = 10;
+         $c = $a + $b;
+         echo $c;
+       // Resposta: D: ERRO!
+    ?>
+    <p>Resposta D</p>
+    <br>
+    <h1>Exercício 8</h1>
+    <?php
+        $pre = "hiper";
+        echo "${pre}texto";
+        // Resposta D
+    ?>
+    <p>Resposta D</p>
+    <br>
+    <h1>Exercício 9</h1>
+    <?php
+        $x = "P";
+        $y = "H";
+        $z = "$x$y$x";
+        echo $z;
+        // Resposta B: PHP
+     ?>
+    <p>Resposta B</p>
+</body>
+</html>
