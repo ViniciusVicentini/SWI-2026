0115f73d635159788e6c097b1477bbe6eb1cf975
ViniciusVicentini <vinisvicentini@gmail.com>
2026-06-18 11:36:37 -0300

    Delete aula_09_funcoes directory

--- aula_09_funcoes/exemplo_03.php
+++
@@ -1,9 +0,0 @@
-<?php
-        function msg($nome) {
-        return "Bom dia $nome :D <br>";
-    }
-
-    $retorno = msg("Cleber");
-
-    echo msg("Cleber");
-?>
--- aula_09_funcoes/exercicio_3.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function calcularAutonomia($capacidadeBateria, $consumoPorMinuto) {
-        $tempo = $capacidadeBateria / $consumoPorMinuto;
-       return "Tempo estimado de voo: $tempo minutos"; 
-    }
-
-    echo calcularAutonomia(90, 45);
-?>
